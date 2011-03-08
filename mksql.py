dab742ae71de501c22b30e33c47dfb8a8febae78
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-03-08 21:41:52 +0900

    Add script to generate SQL from CSV files.

---
+++ mksql.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+import os
+import re
+import sys
+import string
+import sqlite3
+
+DATA_DIR = "data/"
+#DB_NAME = ":memory:"
+DB_NAME = "ares.db"
+DEBUG = False
+#DEBUG = True
+DEBUG_ALLROWS = False
+#DEBUG_ALLROWS = True
+
+
+class CSVParseException(Exception):
+    pass
+
+def typechange(x, typestring, isnull):
+    null = "NULL" if isnull else 0
+    if typestring == "text":
+        return x.decode("utf-8")
+    elif typestring == "integer":
+        return int(x) if x != "" else null
+    elif typestring == "trash":
+        return None
+    else:
+        return x
+
+
+def mktable_from_csv(db, tablename, filename = None):
+    if DEBUG: print "Start Table: ", tablename
+    if filename is None: filename = DATA_DIR + tablename + ".csv"
+    # Open CSV file.
+    csv = open(filename, 'r')
+
+    # Read header.
+    # Header format is like that shown below.
+    # id:integer:p, name:text:un, other:integer::foo[name]
+    # Header is separated with comma.
+    # First one means 'id' column is integer and primary key.
+    # Second one means 'name' column is text and UNIQUE NOT NULL.
+    # Third one means 'other' column is integer and
+    # 'foo' table's 'name' column value is written.
+    # This program will find foo(name) and insert foo(id) value.
+    headline = [i.split(':') for i in csv.readline()[:-1].split(',')]
+    names = tuple(i[0] for i in headline)
+    types = tuple(i[1] for i in headline)
+    constraint = tuple(i[2] if len(i) > 2 else None for i in headline)
+    foreignkey = tuple(re.match(r"(.*?)\[(.*?)\]", i[3]).groups()
+                       if len(i) > 3 else None for i in headline)
+    primarykey = tuple(col for col, opt in zip(names, constraint)
+                       if opt is not None and string.find(opt, 'p') >= 0)
+    options = tuple(((" NOT NULL" if string.find(opt, 'n') >= 0 else " ")+
+                     (" UNIQUE"   if string.find(opt, 'u') >= 0 else " "))
+                    if opt is not None else " "
+                    for opt in constraint)
+
+    # Read rows.
+    rawrows = [tuple(i[:-1].split(',')) for i in csv.readlines()]
+    rows = []
+    if DEBUG: print names, types, constraint, foreignkey, options
+    for row in rawrows:
+        cols = []
+        for i, col in enumerate(row):
+            if foreignkey[i] is None:
+                x = typechange(col, types[i],
+                               constraint[i] is None or
+                               string.find(constraint[i], 'n') == -1)
+            elif string.upper(col) == "NULL" or col == "":
+                x = "NULL"
+            else:
+                cur = db.execute("SELECT id FROM %s WHERE %s='%s'" %
+                                 (foreignkey[i][0], foreignkey[i][1], col)
+                                 ).fetchone()
+                if cur is not None and len(cur) > 0: x = cur[0]
+                else:
+                    raise CSVParseException("value '%s' is not found in %s.%s"
+                                            % (col,
+                                               foreignkey[i][0],
+                                               foreignkey[i][1]))
+            if x is not None: cols.append(x if x != "NULL" else None)
+        rows.append(tuple(cols))
+    if DEBUG_ALLROWS:
+        for row in rows:
+            for i in row:
+                print i, "@",
+            print ""
+
+    # Delete trash column.
+    trash = lambda l: tuple(x for i,x in enumerate(l) if types[i] != "trash")
+    names = trash(names)
+    options = trash(options)
+    types = trash(types)
+
+    original_cols = names
+
+    # Add primary key 'id' if there is not any primary key.
+    if len(primarykey) == 0:
+        names = ("id", ) + names
+        types = ("INTEGER", ) + types
+        constraint = ("p", ) + constraint
+        foreignkey = (None, ) + foreignkey
+        options = (" PRIMARY KEY AUTOINCREMENT", ) + options
+
+    # Create DB.
+    sql = ("CREATE TABLE %s(" % tablename +
+           ",".join(i+" "+j+" "+k for i,j,k in zip(names, types, options)) +
+           (", PRIMARY KEY (%s)" % ",".join(i for i in primarykey)
+            if len(primarykey) > 0 else "") +
+           "".join(", FOREIGN KEY(%s) REFERENCES %s(id)" %
+                   (names[i], fkey[0])
+                   for i, fkey in enumerate(foreignkey)
+                   if fkey is not None) +
+           ");")
+    if DEBUG: print sql
+    db.execute(sql)
+
+    # Insert data.
+    sql = ("INSERT INTO %s (%s) values (%s)" %
+           (tablename,
+            ",".join(i for i in original_cols),
+            ",".join('?' for i in range(0, len(original_cols)))))
+    if DEBUG: print sql
+    #db.executemany(sql, rows)
+    for row in rows:
+        try:
+            db.execute(sql, row)
+        except:
+            print >>sys.stderr, "Error occurred: ", row
+            raise
+
+
+def mksqldb():
+    if os.path.exists(DB_NAME): os.unlink(DB_NAME)
+    db = sqlite3.connect(DB_NAME)
+    db.execute("PRAGMA foreign_keys = ON;")
+    for i in ("company", "station", "line", "urban", "kilo"):
+        mktable_from_csv(db, i)
+    db.commit()
+    db.close()
+
+def main():
+    mksqldb()
+
+if(__name__ == "__main__"):
+    main()
