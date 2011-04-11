9248fb46508e1d523e0e7f817fa0126360768262
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-04-11 18:05:34 +0900

    Change test.cpp's try-catch and add line example.

--- src/test.cpp
+++ src/test.cpp
@@ -1,46 +1,66 @@
 #include <iostream>
 #include <locale>
+#include <memory>
+#include <stdexcept>
 #include <cstdlib>
 #include <boost/foreach.hpp>
 #include "cdatabase.h"
 
 void testdb(const char * filename)
 {
+  std::unique_ptr<ares::CDatabase> db;
   try
     {
-      ares::CDatabase db(filename);
-      ares::station_vector result;
-      std::cout << "name:PREFIX:鹿児島" << std::endl;
-      db.search_station_with_name("鹿児島", ares::SEARCH_PREFIX, result);
-      BOOST_FOREACH(ares::station_id_t & e, result)
-	{
-	  std::cout << e << '\t' << db.get_station_name(e) << std::endl;
-	}
-      result.clear();
-      std::cout << "yomi:PREFIX:はっさむ" << std::endl;
-      db.search_station_with_yomi("はっさむ", ares::SEARCH_PREFIX, result);
-      BOOST_FOREACH(ares::station_id_t & e, result)
-	{
-	  std::cout << e << '\t' << db.get_station_name(e) << std::endl;
-	}
-      result.clear();
-      std::cout << "denryaku:EXACT:ミフ" << std::endl;
-      db.search_station_with_denryaku("ミフ", ares::SEARCH_EXACT, result);
-      BOOST_FOREACH(ares::station_id_t & e, result)
-	{
-	  std::cout << e << '\t' << db.get_station_name(e) << std::endl;
-	}
-      result.clear();
-      std::cout << "denryaku:PREFIX:セカオ" << std::endl;
-      db.search_station_with_denryaku("セカオ", ares::SEARCH_PREFIX, result);
-      BOOST_FOREACH(ares::station_id_t & e, result)
-	{
-	  std::cout << e << '\t' << db.get_station_name(e) << std::endl;
-	}
+      db.reset(new ares::CDatabase(filename));
     }
   catch(ares::IOException & e)
     {
       exit(EXIT_FAILURE);
+    }
+  ares::station_vector sresult;
+  std::cout << "name:PREFIX:鹿児島" << std::endl;
+  db->search_station_with_name("鹿児島", ares::SEARCH_PREFIX, sresult);
+  BOOST_FOREACH(ares::station_id_t & e, sresult)
+    {
+      std::cout << e << '\t' << db->get_station_name(e) << std::endl;
+    }
+  sresult.clear();
+  std::cout << "yomi:PREFIX:はっさむ" << std::endl;
+  db->search_station_with_yomi("はっさむ", ares::SEARCH_PREFIX, sresult);
+  BOOST_FOREACH(ares::station_id_t & e, sresult)
+    {
+      std::cout << e << '\t' << db->get_station_name(e) << std::endl;
+    }
+  sresult.clear();
+  std::cout << "denryaku:EXACT:ミフ" << std::endl;
+  db->search_station_with_denryaku("ミフ", ares::SEARCH_EXACT, sresult);
+  BOOST_FOREACH(ares::station_id_t & e, sresult)
+    {
+      std::cout << e << '\t' << db->get_station_name(e) << std::endl;
+    }
+  sresult.clear();
+  std::cout << "denryaku:PREFIX:セカオ" << std::endl;
+  db->search_station_with_denryaku("セカオ", ares::SEARCH_PREFIX, sresult);
+  BOOST_FOREACH(ares::station_id_t & e, sresult)
+    {
+      std::cout << e << '\t' << db->get_station_name(e) << std::endl;
+    }
+  ares::station_vector lresult;
+  std::cout << "connect:東海道\n";
+  db->search_line_with_name("東海道", ares::SEARCH_EXACT, lresult);
+  try
+    {
+      ares::line_id_t l = lresult.at(0);
+      lresult.clear();
+      db->search_connect_line(l, lresult);
+      BOOST_FOREACH(ares::line_id_t & e, lresult)
+	{
+	  std::cout << e << '\t' << db->get_line_name(e) << std::endl;
+	}
+    }
+  catch(const std::out_of_range & e)
+    {
+      std::cerr << "Line not found.\n";
     }
 }
 
