1766e1b60f0372fda38b1a39156932985ea9ec31
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-12-07 02:57:16 +0900

    Move enum to common header.

--- src/ckilo.h
+++ src/ckilo.h
@@ -7,38 +7,6 @@
 
 namespace ares
 {
-  /**
-   * @~
-   * 運賃体系上の会社の列挙体.
-   */
-  enum COMPANY_TYPE {
-    COMPANY_HONSHU,
-    COMPANY_HOKKAIDO,
-    COMPANY_KYUSHU,
-    COMPANY_SHIKOKU,
-    MAX_COMPANY_TYPE,
-  };
-
-  /**
-   * @~
-   * 幹線と地方交通線の列挙体.
-   */
-  enum LINE_TYPE {
-    LINE_LOCAL,
-    LINE_MAIN,
-    MAX_LINE_TYPE,
-  };
-
-  /**
-   * @~
-   * 実キロと擬制キロの列挙体.
-   */
-  enum KILO_TYPE {
-    KILO_REAL,
-    KILO_FAKE,
-    MAX_KILO_TYPE,
-  };
-
   namespace
   {
     inline size_t linetype(bool is_main)
