ae2f722b37dc14845c71650824a885059a4ee0fb
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2012-01-02 02:37:23 +0900

    Implemented Find Dialog.

--- wxsrc/listframe.cpp
+++ wxsrc/listframe.cpp
@@ -1,19 +1,22 @@
 #include "listframe.h"
 #include "main.h"
-#include <wx/sizer.h>
-#include <wx/button.h>
 #include <wx/menu.h>
+#include <wx/sizer.h>
+#include <wx/arrstr.h>
 #include <wx/msgdlg.h>
+#include <wx/aboutdlg.h>
+#include <wx/choicdlg.h>
 #include <vector>
 #include <utility>
 #include <exception>
 #include "util.hpp"
-#include "cstation.h"
 #include "cdatabase.h"
 
 BEGIN_EVENT_TABLE(AresListFrame, wxFrame)
   EVT_MENU(MENU_ABOUT, AresListFrame::OnAbout)
   EVT_MENU(MENU_QUIT, AresListFrame::OnQuit)
+  EVT_MENU(MENU_SEARCH_STATION, AresListFrame::OnSearchStationDialog)
+  EVT_MENU(MENU_SEARCH_LINE, AresListFrame::OnSearchLineDialog)
   EVT_LISTBOX(LINE_LIST_BOX, AresListFrame::OnLineListbox)
 END_EVENT_TABLE();
 
@@ -22,6 +25,20 @@ BEGIN_EVENT_TABLE(AresStationListView, wxListView)
 END_EVENT_TABLE();
 
 BEGIN_EVENT_TABLE(AresLineListBox, wxListBox)
+END_EVENT_TABLE();
+
+BEGIN_EVENT_TABLE(AresSearchDialog, wxDialog)
+  EVT_TEXT(wxID_ANY, AresSearchDialog::OnUpdateQuery)
+  EVT_LISTBOX(SEARCH_RESULT_LIST_BOX, AresSearchDialog::OnSelected)
+  EVT_LISTBOX_DCLICK(SEARCH_RESULT_LIST_BOX, AresSearchDialog::OnDblSelected)
+  EVT_SEARCHCTRL_CANCEL_BTN(wxID_ANY, AresSearchDialog::clearQueryText)
+END_EVENT_TABLE();
+
+BEGIN_EVENT_TABLE(AresSearchStationDialog, AresSearchDialog)
+  EVT_BUTTON(wxID_OK, AresSearchStationDialog::OnOK)
+END_EVENT_TABLE();
+
+BEGIN_EVENT_TABLE(AresSearchLineDialog, AresSearchDialog)
 END_EVENT_TABLE();
 
 namespace
@@ -94,19 +111,34 @@ void AresStationListView::setLineId(ares::line_id_t lineid)
   m_lineid = lineid;
   this->Hide();
   this->DeleteAllItems();
-  std::vector<ares::CStation> result;
-  wxGetApp().getdb()->get_stations_of_line(lineid, result);
-  for(size_t i=0; i < result.size(); ++i)
+  m_stations.clear();
+  wxGetApp().getdb()->get_stations_of_line(lineid, m_stations);
+  for(auto itr=m_stations.begin(); itr != m_stations.end(); ++itr)
   {
     const long tmp = this->InsertItem
-      (i, wxString::FromUTF8(result[i].name.c_str()), 0);
+      (itr-m_stations.begin(), wxString::FromUTF8(itr->name.c_str()), 0);
     this->SetItemData(tmp, 0);
-    this->SetItem(tmp, 1, wxString::FromUTF8(result[i].yomi.c_str()));
-    this->SetItem(tmp, 2, wxString::FromUTF8(result[i].denryaku.c_str()));
-    this->SetItem(tmp, 3, wxString::FromAscii(result[i].realkilo.to_str().c_str()));
-    this->SetItem(tmp, 4, wxString::FromAscii(result[i].fakekilo.to_str().c_str()));
+    this->SetItem(tmp, 1, wxString::FromUTF8(itr->yomi.c_str()));
+    this->SetItem(tmp, 2, wxString::FromUTF8(itr->denryaku.c_str()));
+    this->SetItem(tmp, 3, wxString::FromAscii(itr->realkilo.to_str().c_str()));
+    this->SetItem(tmp, 4, wxString::FromAscii(itr->fakekilo.to_str().c_str()));
   }
   this->Show();
+}
+
+void AresStationListView::setSelection(long item)
+{
+  $.SetItemState(item, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+  $.EnsureVisible(item);
+}
+
+void AresStationListView::setSelectionWithId(ares::station_id_t idval)
+{
+  auto itr = std::find_if(m_stations.begin(), m_stations.end(),
+                          [idval](const ares::CStation & obj) -> bool
+                          { return obj.id == idval; });
+  assert(itr != m_stations.end());
+  $.setSelection(itr - m_stations.begin());
 }
 
 AresLineListBox::AresLineListBox(wxWindow * parent,
@@ -136,12 +168,166 @@ ares::line_id_t AresLineListBox::get_selected_lineid() const
   return $.get_lineid(selected);
 }
 
-// void AresLineListBox::OnSelected(wxListEvent& event)
-// {
-//   wxMessageBox(event.GetText(),
-//                _T("test"),
-//                wxICON_INFORMATION, this);
-// }
+void AresLineListBox::setSelectionWithId(ares::line_id_t idval)
+{
+  auto itr = std::find(m_lineid_vec.begin(), m_lineid_vec.end(), idval);
+  assert(itr != m_lineid_vec.end());
+  $.SetSelection(itr - m_lineid_vec.begin());
+}
+
+AresSearchDialog::AresSearchDialog(wxWindow * parent,
+                                   const wxWindowID id,
+                                   const wxString & title)
+  : wxDialog(parent, id, title),
+    m_query(nullptr), m_button_ok(nullptr), m_button_cancel(nullptr)
+{
+  wxBoxSizer * bottomsizer = new wxBoxSizer(wxHORIZONTAL);
+  m_button_ok = new wxButton(this, wxID_OK);
+  m_button_cancel = new wxButton(this, wxID_CANCEL);
+  bottomsizer->Add(m_button_cancel,
+                   wxSizerFlags().Expand().Border(wxALL, 4).Proportion(0));
+  bottomsizer->Add(m_button_ok,
+                   wxSizerFlags().Expand().Border(wxALL, 4).Proportion(0));
+
+  wxBoxSizer * topsizer = new wxBoxSizer(wxVERTICAL);
+  m_query = new wxSearchCtrl(this, wxID_ANY);
+  m_query->ShowSearchButton(true);
+  m_query->ShowCancelButton(true);
+  // m_query->SetMenu();
+  m_result = new wxListBox(this, SEARCH_RESULT_LIST_BOX);
+  topsizer->Add(m_query,
+                wxSizerFlags().Expand().Border(wxALL, 4).Proportion(0));
+  topsizer->Add(m_result,
+                wxSizerFlags().Expand().Border(wxALL, 4).Proportion(1));
+  topsizer->Add(bottomsizer,
+                wxSizerFlags().Expand().Border(wxALL, 4).Proportion(0));
+  $.SetSizer(topsizer);
+  m_query->SetFocus();
+}
+
+AresSearchDialog::~AresSearchDialog() {}
+
+void AresSearchDialog::clearQueryText(wxCommandEvent& WXUNUSED(event))
+{
+  m_query->SetValue(_T(""));
+}
+
+void AresSearchDialog::OnUpdateQuery(wxCommandEvent& WXUNUSED(event))
+{
+  m_result_id_vec.clear();
+  m_result->Clear();
+
+  // Empty query => none results.
+  if(m_query->IsEmpty()) { return; }
+  $.getQueryResult(m_query->GetValue(), m_result_id_vec);
+  for(auto itr=m_result_id_vec.begin();
+      itr != m_result_id_vec.end(); ++itr)
+  {
+    m_result->Append($.getStringOfQueryId(*itr));
+  }
+}
+
+void AresSearchDialog::OnDblSelected(wxCommandEvent& WXUNUSED(event))
+{
+  wxCommandEvent dummy(wxEVT_COMMAND_BUTTON_CLICKED, $.GetAffirmativeId());
+  m_button_ok->Command(dummy);
+}
+
+wxString AresSearchDialog::getSelectedString()
+{
+  assert(m_result->GetSelection() != wxNOT_FOUND);
+  return m_result->GetString(m_result->GetSelection());
+}
+
+boost::optional<ares::line_id_t> AresSearchDialog::getLine()
+{
+  return m_selected_line;
+}
+
+boost::optional<ares::station_id_t> AresSearchDialog::getStation()
+{
+  return m_selected_station;
+}
+
+AresSearchStationDialog::AresSearchStationDialog(wxWindow * parent)
+  : AresSearchDialog(parent, wxID_ANY, _("Search stations"))
+{
+}
+
+AresSearchStationDialog::~AresSearchStationDialog() {}
+
+void AresSearchStationDialog::OnOK(wxCommandEvent & WXUNUSED(event))
+{
+  // OK
+  if(m_selected_station)
+  {
+    ares::line_vector result;
+    wxGetApp().getdb()->get_lines_of_station(*m_selected_station,
+                                             result);
+    wxArrayString lines;
+    const wxString & station = $.getSelectedString();
+    for(auto itr=result.begin(); itr != result.end(); ++itr)
+    {
+      lines.Add(wxString::FromUTF8(wxGetApp().getdb()->get_line_name(*itr).c_str()));
+    }
+    assert(!lines.IsEmpty());
+    if(lines.GetCount() == 1)
+    {
+      m_selected_line = result[0];
+    }
+    else
+    {
+      wxSingleChoiceDialog dlg(this, wxString::Format(
+                                 _("Choose the line of %s"),
+                                 station.c_str()),
+                               _("Choose line"),
+                               lines);
+      m_selected_line = (wxID_OK == dlg.ShowModal())
+        ? boost::make_optional(result[dlg.GetSelection()])
+        : boost::none;
+    }
+  }
+  $.EndModal(wxID_OK);
+}
+
+void AresSearchStationDialog::OnSelected(wxCommandEvent& WXUNUSED(event))
+{
+  m_selected_station = $.getID();
+}
+
+void AresSearchStationDialog::getQueryResult(const wxString & query,
+                                             std::vector<int> &result)
+{
+  wxGetApp().getdb()->find_stationid(query.ToUTF8(), ares::FIND_PREFIX, result);
+}
+
+wxString AresSearchStationDialog::getStringOfQueryId(const int id)
+{
+  return wxString::FromUTF8(wxGetApp().getdb()->get_station_name(id).c_str());
+}
+
+AresSearchLineDialog::AresSearchLineDialog(wxWindow * parent)
+  : AresSearchDialog(parent, wxID_ANY, _("Search lines"))
+{
+}
+
+AresSearchLineDialog::~AresSearchLineDialog() {}
+
+void AresSearchLineDialog::OnSelected(wxCommandEvent& WXUNUSED(event))
+{
+  m_selected_line = $.getID();
+}
+
+void AresSearchLineDialog::getQueryResult(const wxString & query,
+                                             std::vector<int> &result)
+{
+  wxGetApp().getdb()->find_lineid(query.ToUTF8(), ares::FIND_PREFIX, result);
+}
+
+wxString AresSearchLineDialog::getStringOfQueryId(const int id)
+{
+  return wxString::FromUTF8(wxGetApp().getdb()->get_line_name(id).c_str());
+}
 
 AresListFrame::AresListFrame(const wxString & title,
                              const wxPoint &pos,
@@ -152,11 +338,16 @@ AresListFrame::AresListFrame(const wxString & title,
   wxMenu *menuFile = new wxMenu;
   menuFile->Append(MENU_QUIT, _("E&xit\tCtrl-Q"));
 
+  wxMenu *menuSearch = new wxMenu;
+  menuSearch->Append(MENU_SEARCH_STATION, _("Search &Station\t/"));
+  menuSearch->Append(MENU_SEARCH_LINE, _("Search &Line\t?"));
+
   wxMenu *menuHelp = new wxMenu;
   menuHelp->Append(MENU_ABOUT, _("&About...\tF1"));
 
   wxMenuBar *menu_bar = new wxMenuBar;
   menu_bar->Append(menuFile, _("&File"));
+  menu_bar->Append(menuSearch, _("&Search"));
   menu_bar->Append(menuHelp, _("&Help"));
   SetMenuBar(menu_bar);
 
@@ -186,10 +377,43 @@ void AresListFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
 
 void AresListFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 {
-  wxMessageBox(_("wxAres for JR fare calculate\n"
-                 "(c) liquid_amber 2010-2011"),
-               _("About wxAres"),
-               wxICON_INFORMATION, this);
+  wxAboutDialogInfo aboutInfo;
+  aboutInfo.SetName(_T("wxAres"));
+  // aboutInfo.SetVersion("");
+  aboutInfo.SetDescription(_("wxAres for JR fare calculate"));
+  aboutInfo.SetCopyright(_T("(c) 2010-2011"));
+  aboutInfo.SetWebSite(_T("http://github.com/liquidamber/ares/"));
+  aboutInfo.AddDeveloper(_T("liquid_amber"));
+
+  wxAboutBox(aboutInfo);
+}
+
+template<class DialogType>
+void AresListFrame::OnSearchDialog()
+{
+  DialogType dlg(this);
+  if(wxID_OK == dlg.ShowModal())
+  {
+    if(auto line = dlg.getLine())
+    {
+      m_lineList->setSelectionWithId(*line);
+      $.setLineId(*line);
+      if(auto station = dlg.getStation())
+      {
+        m_stationList->setSelectionWithId(*station);
+      }
+    }
+  }
+}
+
+void AresListFrame::OnSearchStationDialog(wxCommandEvent& WXUNUSED(event))
+{
+  OnSearchDialog<AresSearchStationDialog>();
+}
+
+void AresListFrame::OnSearchLineDialog(wxCommandEvent& WXUNUSED(event))
+{
+  OnSearchDialog<AresSearchLineDialog>();
 }
 
 void AresListFrame::OnLineListbox(wxCommandEvent& event)
--- wxsrc/listframe.h
+++ wxsrc/listframe.h
@@ -1,9 +1,14 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
 #include <wx/listbox.h>
 #include <wx/listctrl.h>
+#include <wx/srchctrl.h>
 #include <vector>
+#include <boost/optional.hpp>
 #include "ares.h"
+#include "cstation.h"
 
 class AresStationListView : public wxListView
 {
@@ -12,6 +17,7 @@ public:
                       const wxWindowID id,
                       const wxPoint &pos=wxDefaultPosition,
                       const wxSize &size=wxDefaultSize);
+  virtual ~AresStationListView() {}
 
   void OnSelected(wxListEvent& event);
   // void OnDeselected(wxListEvent& event);
@@ -20,11 +26,15 @@ public:
 
   void setLineId(ares::line_id_t lineid);
 
+  void setSelection(long item);
+  void setSelectionWithId(ares::station_id_t idval);
+
 private:
   DECLARE_NO_COPY_CLASS(AresStationListView);
   DECLARE_EVENT_TABLE();
 
   ares::line_id_t m_lineid;
+  std::vector<ares::CStation> m_stations;
 };
 
 class AresLineListBox : public wxListBox
@@ -32,14 +42,83 @@ class AresLineListBox : public wxListBox
 public:
   AresLineListBox(wxWindow * parent,
                   const wxWindowID id);
+  virtual ~AresLineListBox() {}
 
   ares::line_id_t get_lineid(int selected) const;
 
   ares::line_id_t get_selected_lineid() const;
 
+  void setSelectionWithId(ares::line_id_t idval);
+
 private:
   std::vector<ares::line_id_t> m_lineid_vec;
   DECLARE_NO_COPY_CLASS(AresLineListBox);
+  DECLARE_EVENT_TABLE();
+};
+
+class AresSearchDialog : public wxDialog
+{
+public:
+  AresSearchDialog(wxWindow * parent,
+                 const wxWindowID id,
+                 const wxString & title);
+  virtual ~AresSearchDialog();
+  void clearQueryText(wxCommandEvent & event);
+  void OnUpdateQuery(wxCommandEvent & event);
+  virtual void OnSelected(wxCommandEvent & event) = 0;
+  void OnDblSelected(wxCommandEvent & event);
+  virtual void getQueryResult(const wxString & query,
+                              std::vector<int> &result) = 0;
+  wxString getSelectedString();
+  virtual wxString getStringOfQueryId(const int id) = 0;
+  boost::optional<ares::line_id_t> getLine();
+  boost::optional<ares::station_id_t> getStation();
+
+private:
+  wxSearchCtrl * m_query;
+  wxListBox * m_result;
+  wxButton * m_button_ok, * m_button_cancel;
+
+  std::vector<int> m_result_id_vec;
+
+protected:
+  boost::optional<ares::line_id_t> m_selected_line;
+  boost::optional<ares::station_id_t> m_selected_station;
+
+  int getID()
+  {
+    return m_result_id_vec[m_result->GetSelection()];
+  }
+
+  DECLARE_EVENT_TABLE();
+};
+
+class AresSearchStationDialog : public AresSearchDialog
+{
+public:
+  explicit AresSearchStationDialog(wxWindow * parent);
+  virtual ~AresSearchStationDialog();
+  void OnOK(wxCommandEvent & event);
+  virtual void OnSelected(wxCommandEvent& event);
+  virtual void getQueryResult(const wxString & query,
+                              std::vector<int> &result);
+  virtual wxString getStringOfQueryId(const int id);
+
+private:
+  DECLARE_EVENT_TABLE();
+};
+
+class AresSearchLineDialog : public AresSearchDialog
+{
+public:
+  explicit AresSearchLineDialog(wxWindow * parent);
+  virtual ~AresSearchLineDialog();
+  virtual void OnSelected(wxCommandEvent& event);
+  virtual void getQueryResult(const wxString & query,
+                              std::vector<int> &result);
+  virtual wxString getStringOfQueryId(const int id);
+
+private:
   DECLARE_EVENT_TABLE();
 };
 
@@ -54,6 +133,8 @@ public:
   // notifications
   void OnQuit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
+  void OnSearchStationDialog(wxCommandEvent& event);
+  void OnSearchLineDialog(wxCommandEvent& event);
   void OnLineListbox(wxCommandEvent& event);
 
   DECLARE_NO_COPY_CLASS(AresListFrame);
@@ -65,6 +146,8 @@ private:
   AresStationListView * m_stationList;
   ares::line_id_t m_lineid;
 
+  template<class SearchDialog>
+  void OnSearchDialog();
   void setLineId(ares::line_id_t lineid);
 };
 
@@ -72,6 +155,9 @@ enum
 {
   LINE_LIST_BOX = 100,
   STATION_LIST_VIEW,
+  SEARCH_RESULT_LIST_BOX,
+  MENU_SEARCH_STATION,
+  MENU_SEARCH_LINE,
   MENU_ABOUT = wxID_ABOUT,
   MENU_QUIT = wxID_EXIT,
 };
